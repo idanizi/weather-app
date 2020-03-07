ca349ee3105477086c0feb006b7c1cd52c1b13ed
Idan Izicovich <idanizi@gmail.com>
2020-03-07 03:11:49 +0200

    Updates

---
+++ precache-manifest.cdb7f9217aca7f0f3191b70f0b7ca8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932c5da2eaac9228d9b9eafcc1c01492",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "806beca90b31a0aa1caf",
+    "url": "/weather-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1557b648a528f7add247",
+    "url": "/weather-app/static/css/main.393619dc.chunk.css"
+  },
+  {
+    "revision": "806beca90b31a0aa1caf",
+    "url": "/weather-app/static/js/2.cfed05ca.chunk.js"
+  },
+  {
+    "revision": "606177481fafe62e0bcba9ace9a19fff",
+    "url": "/weather-app/static/js/2.cfed05ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1557b648a528f7add247",
+    "url": "/weather-app/static/js/main.e953762b.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
